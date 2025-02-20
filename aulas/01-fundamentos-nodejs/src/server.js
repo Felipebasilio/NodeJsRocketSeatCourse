0807c9c105cc5b4ff092648649cb75fb7e4b1a9f
Felipe Basilio <fcb.97@hotmail.com>
2025-02-20 02:19:11 -0300

    [FINISHED][MOD1/CLASS3]: Streams at Node.js

--- aulas/01-fundamentos-nodejs/src/server.js
+++ aulas/01-fundamentos-nodejs/src/server.js
@@ -1,47 +1,26 @@
 import http from 'node:http'
+import { json } from './middlewares/json.js'
 
-// - Criar usuários
-// - Listagem usuários
-// - Edição de usuários
-// - Remoção de usuários
-
-// - HTTP
-//   - Método HTTP
-//   - URL
-
-// GET, POST, PUT, PATCH, DELETE
-
-// GET => Buscar um recurso do back-end
-// POST => Criar um recurso no back-end
-// PUT => Atualizar um recurso no back-end
-// PATCH => Atualizar uma informação específica de um recurso no back-end
-// DELETE => Deletar um recurso do back-end
-
-// GET /users => Buscando usuários no banc-end
-// POST /users => Criar um usuário no back-end
-
-// Stateful - Stateless
-
-// Cabeçalhos (Requisição/resposta) => Metadados
-
-// HTTP Status Code
 
 const users = []
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const { method, url } = req
+
+  await json(req, res)
 
   if (method === 'GET' && url === '/users') {
     return res
-      .setHeader('Content-type', 'application/json')
       .end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
+    const { name, email } = req.body
+
     users.push({
       id: 1,
-      name: 'John Doe',
-      email: 'johndoe@example.com',
+      name,
+      email,
     })
 
     return res.writeHead(201).end()
@@ -50,6 +29,4 @@ const server = http.createServer((req, res) => {
   return res.writeHead(404).end()
 })
 
-server.listen(3333, () => {
-  console.log('Server is running at http://localhost:3333/');
-}); 
+server.listen(3333)
