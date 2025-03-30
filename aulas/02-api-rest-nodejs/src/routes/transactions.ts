c83c225f7cf2746f4100d740a99ec6588f43f208
Felipe Basilio <fcb.97@hotmail.com>
2025-03-30 08:34:57 -0300

    Class 2: Added some routes using knex, get, getId, and post

---
+++ aulas/02-api-rest-nodejs/src/routes/transactions.ts
@@ -0,0 +1,47 @@
+import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
+import { knex } from '../database'
+import { randomUUID } from 'node:crypto'
+
+// Every Fastify plçugin got to be an async function
+export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async (request, reply) => {
+    const transactions = await knex('transactions').select()
+
+    return reply.status(200).send({
+      transactions,
+    })
+  })
+
+  app.get('/:id', async (request, reply) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+    const { id } = getTransactionParamsSchema.parse(request.params)
+    const transaction = await knex('transactions').where('id', id).first()
+
+    return reply.status(200).send({
+      transaction,
+    })
+  })
+
+  app.post('/', async (request, reply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
+
+    const { title, amount, type } = createTransactionBodySchema.parse(
+      request.body,
+    )
+
+    await knex('transactions').insert({
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+
+    return reply.status(201).send()
+  })
+}
