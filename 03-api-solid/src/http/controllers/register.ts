56f30095620ccb41d52c3a6702b750b2f811f7b4
Felipe Basilio <fcb.97@hotmail.com>
2025-04-08 21:40:27 -0300

    [Chapter 3 - Third class]: Added use cases, controllers, repository pattern, dependency inversion, repo interface and global error handlers

--- 03-api-solid/src/http/controllers/register.ts
+++ 03-api-solid/src/http/controllers/register.ts
@@ -1,6 +1,10 @@
 import { FastifyRequest, FastifyReply } from 'fastify'
+import { hash } from 'bcryptjs'
 import { prisma } from '@/lib/prisma'
 import { z } from 'zod'
+import { RegisterUseCase } from '@/use-cases/register';
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository';
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists';
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -11,13 +15,24 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
   const { name, email, password } = registerBodySchema.parse(request.body)
 
-  await prisma.user.create({
-    data: {
+  try {
+    const usersRepository = new PrismaUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+
+    await registerUseCase.execute({
       name,
       email,
-      password_hash: password,
-    },
-  })
+      password,
+    })
+  } catch (error) {
+    if (error instanceof UserAlreadyExistsError) {
+      return reply.status(409).send(
+        { message: error.message },
+      )
+    }
+
+    throw error
+  }
 
   return reply.status(201).send()
 }
